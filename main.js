f0823b29d676c77a11681c26723c8e9dd931655f
saabismi <vilho.syvahuoko@gmail.com>
2020-10-06 12:14:38 +0300

    add basic electron files

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile("index.html");
+
+}
+
+app.whenReady().then(createWindow);
