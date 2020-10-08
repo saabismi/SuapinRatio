414e8ba6466b8e93c5d199b8b385433e79f041a6
Hakaponttoauto <60978162+Hakaponttoauto@users.noreply.github.com>
2020-10-08 23:23:54 +0300

    Basic interaction
    Done basic music searching and playing functions.

--- player.js
+++ player.js
@@ -1,5 +1,5 @@
 const ytdl = require("ytdl-core");
-const ytsr = require('ytsr');
+const ytsr = require("ytsr");
 
 const fs = require("fs");
 const audio = new Audio();
@@ -7,20 +7,32 @@ let stream = fs.createWriteStream("temp.mp4");
 
 let url = ""
 
-function loadVideo(url) {
+function loadVideo(url, title) {
 	ytdl(url,{ filter: format => format.container === 'mp4' }).pipe(stream);
 	stream.on("close", ()=>{
 	    audio.src = "temp.mp4";
 	    audio.play();
+			document.getElementById("playing").innerHTML = title;
 	    audio.onended = () => {
 	        fs.unlink("temp.mp4", ()=>{});
+					document.getElementById("playing").innerHTML="";
 	    }
 	})
 }
 
 function search(string) {
-	document.getElementById("main-container").append("Results:");
-	ytsr(string).then(result => {
-		document.getElementById("main-container").append(result.items[0].title);
+	var main = document.getElementById("main-container");
+  while (main.firstChild) {
+    main.removeChild(main.lastChild);
+  }
+	ytsr(string, {limit: 3}).then(result => {
+		result.items.forEach((item, i) => {
+			var button = document.createElement("BUTTON");
+			button.setAttribute("onClick",`loadVideo( '${item.link}' , '${item.title}' ) `);
+			button.setAttribute("class", "searchResult");
+			main.appendChild(button);
+			button.innerHTML=`<img class="thumbnail" src='${item.thumbnail}'>`;
+		});
+
 	});
 }
