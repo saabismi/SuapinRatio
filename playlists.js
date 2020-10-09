0e0a29a43e13ac403084b910029cae03c2df9295
saabismi <vilho.syvahuoko@gmail.com>
2020-10-09 13:59:21 +0300

    starting developing the playlist feature

---
+++ playlists.js
@@ -0,0 +1,108 @@
+class playlist {
+    constructor(name, owner, duration) {
+        this.listname = name;
+        this.listowner = owner;
+        this.listduration = duration;
+    }
+}
+
+testi = new playlist("Hyviä biisejä", "admin", "41:24");
+
+
+function newList() {
+
+    var name = prompt("Syötä soittolistalle nimi:");
+    var owner = "admin";
+    var duration = "13:58";
+
+    name = new playlist(name, owner, duration);
+}
+
+function addToList() {
+
+    var list = "Hyviä biisejä";
+    var song = prompt("Anna syötettävän kappaleen youtube-linkki:");
+
+    list.push(song);
+}
+
+console.log("Listan " + testi.listname + " kesto on " + testi.listduration);
+
+
+function listaaLista(lista) {
+    var lista;
+
+    for(let i = 0; i < lista.length; i++) {
+        console.log(testi[i]);
+    }
+}
+
+listaaLista(testi);
+
+let playlists = [
+    //example 1
+    {
+        name: "testi",
+        icon: "https://google.com",
+        songs: [
+            {
+                author: "pentti",
+                url: "https://google.com"
+            },
+            {
+                author: "pentti2",
+                url: "https://google.com"
+            }
+
+        ]
+    },
+    //example 2
+    {
+        name: "testi",
+        icon: "https://google.com",
+        songs: [
+            {
+                author: "pentti",
+                url: "https://google.com"
+            },
+            {
+                author: "pentti2",
+                url: "https://google.com"
+            }
+
+        ]
+    },
+]
+//To add:
+playlists.push({
+    name: "testi",
+    icon: "https://google.com",
+    songs: [
+        {
+            author: "pentti",
+            url: "https://google.com"
+        },
+        {
+            author: "pentti2",
+            url: "https://google.com"
+        }
+
+    ]
+})
+
+//Add to playlist
+playlists[<index>].push({
+    name: "testi",
+    icon: "https://google.com",
+    songs: [
+        {
+            author: "pentti",
+            url: "https://google.com"
+        },
+        {
+            author: "pentti2",
+            url: "https://google.com"
+        }
+
+    ]
+})
