6e5e02b5d58470652567ab661462a68c4545b802
Hakaponttoauto <60978162+Hakaponttoauto@users.noreply.github.com>
2020-10-09 22:48:36 +0300

    Discord RPC
    Added Discord RPC. That is all.
    (Player is still not fixed)

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const { app, BrowserWindow } = require("electron");
-
+let win;
 
 function createWindow() {
 
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 1000,
         height: 820,
-        icon: __dirname + "/resources/icons/icon_hires.png",
+        icon: __dirname + "/resources/icons/logo.png",
         webPreferences: {
             nodeIntegration: true
 
@@ -17,4 +17,38 @@ function createWindow() {
 
 }
 
+
 app.whenReady().then(createWindow);
+
+const DiscordRPC = require('discord-rpc');
+const clientId = '764187560418344980';
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+const startTimestamp = new Date();
+
+
+async function setActivity() {
+  if (!rpc || !win) {
+    return;
+  }
+
+	const title = await win.webContents.executeJavaScript('window.playing');
+
+  rpc.setActivity({
+    details: 'Listening to music',
+    state: title,
+    startTimestamp,
+    instance: false,
+		largeImageKey: 'logo'
+  });
+}
+
+rpc.on('ready', () => {
+  setActivity();
+
+  // activity can only be set every 15 seconds
+  setInterval(() => {
+    setActivity();
+  }, 15e3);
+});
+
+rpc.login({ clientId }).catch(console.error);
--- player.js
+++ player.js
@@ -1,35 +1,46 @@
 const ytdl = require("ytdl-core");
 const ytsr = require("ytsr");
-
 const fs = require("fs");
+window.playing = "Currently not playing anything";
 
 let url = ""
-let audio = new Audio();
+let lists
+
 
 function loadVideo(url, title) {
 	fs.unlink("temp.mp4", ()=>{});
-	let stream = fs.createWriteStream("temp.mp4");
+	let audio = new Audio();
+	audio.src = "";
+
 	document.getElementById("loading").style.display = "block";
+
+	let stream = fs.createWriteStream("temp.mp4");
 	ytdl(url,{ filter: format => format.container === 'mp4' }).pipe(stream);
 	stream.on("close", ()=>{
+
 	    audio.src = "temp.mp4";
 			audio.time = 0
 	    audio.play();
+
 			document.getElementById("playing").innerHTML = title;
 			document.getElementById("loading").style.display = "none";
+			window.playing = title;
+
 	    audio.onended = () => {
 	        fs.unlink("temp.mp4", ()=>{});
+
 					document.getElementById("playing").innerHTML="";
+					window.playing = "Currently not playing anything";
 	    }
 	})
 }
 
 function search(string) {
-	var main = document.getElementById("results");
+	let main = document.getElementById("results");
 	main.innerHTML = "<img src='loading.gif' id='loading'>";
 	ytsr(string, {limit: 3}).then(result => {
 		result.items.forEach((item, i) => {
-			var button = document.createElement("BUTTON");
+			let button = document.createElement("BUTTON");
 			button.setAttribute("onClick",`loadVideo( '${item.link}' , '${item.title}' ) `);
 			button.setAttribute("class", "searchResult");
 			main.appendChild(button);
