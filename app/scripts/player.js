b5e7e56532ec7ec5456fd014ee13b0f5815d1301
Hakaponttoauto <60978162+Hakaponttoauto@users.noreply.github.com>
2020-10-12 19:25:54 +0300

    playlist basics
    (player fixed too)

--- app/scripts/player.js
+++ app/scripts/player.js
@@ -3,50 +3,137 @@ const ytsr = require("ytsr");
 const fs = require("fs");
 window.playing = "Currently not playing anything";
 
-let url = ""
-let lists
+let audio = new Audio();
 
+let lists = {
+	search: {
+		name: "search",
+		target: "results",
+		items: [],
+		now: 0,
+		playing: false,
+	},
+	playing: {
+		name: "playing",
+		target: "currentList",
+		items: [],
+		now: 0,
+		playing: false,
+	},
+};
 
-function loadVideo(url, title) {
-	fs.unlink("temp.mp4", ()=>{});
-	let audio = new Audio();
-	audio.src = "";
+function removeTrack(list, track) {
+	lists[list].items.splice(track, 1);
+	playTrack(list)
+}
 
-	document.getElementById("loading").style.display = "block";
+function playTrack(list, track) {
+	if (!(track === undefined) || !lists[list].playing) {
+		if (!(track === undefined)) {lists[list].now=track;}
+		document.getElementById('playing').innerText = lists[list].items[lists[list].now].title
+		window.playing = lists[list].items[lists[list].now].title;
 
-	let stream = fs.createWriteStream("temp.mp4");
-	ytdl(url,{ filter: format => format.container === 'mp4' }).pipe(stream);
-	stream.on("close", ()=>{
+		audio.src = `../${lists[list].items[lists[list].now].source}`;
+		audio.time = 0;
+		audio.play();
+		lists[list].playing = true;
+		audio.onended = () => {
+			if (lists[list].now+1 >= lists[list].items.lenght) {
+				lists[list].now = 0;
+				lists[list].playing = false;
+				document.getElementById('playing').innerText = " "
+				window.playing = "Currently not playing anything";
+				return;
+			}
+			playTrack('playing', lists[list].now+1);
+		}
+	}
+}
 
-	    audio.src = "temp.mp4";
-			audio.time = 0
-	    audio.play();
+function saveList(name) {
+	lists[name]=lists.playing;
+	lists[name].name=name;
+	printLists("playlists");
+}
 
-			document.getElementById("playing").innerHTML = title;
-			document.getElementById("loading").style.display = "none";
-			window.playing = title;
+function playList(list) {
+	lists.playing.items = [];
+	lists.playing.items = lists[list].items;
+}
 
-	    audio.onended = () => {
-	        fs.unlink("temp.mp4", ()=>{});
-
-					document.getElementById("playing").innerHTML="";
-					window.playing = "Currently not playing anything";
+function printLists(target) {
+	div = document.getElementById(target);
+	for (let key in lists) {
+	    if (p.hasOwnProperty(key)) {
+			button = document.createElement("BUTTON");
+			button.setAttribute("class","playlist");
+			button.setAttribute("onClick", `playList(${key});`);
+	        div.appendChild(button);
+			button.innerText = lists[key].name;
 	    }
-	})
+	}
+}
+
+
+function loadVideo(from, id, to) {
+	let item = lists[from].items[id];
+	let destination = `tracks/${item.title.replace(/\ /g,'_')}.mp4`;
+	let track = {
+		title: item.title,
+		url: item.link,
+		thumbnail: item.thumbnail,
+		duration: item.duration,
+		source: destination,
+		playing: false,
+	};
+	lists[to].items.push(track);
+	if (!fs.existsSync(destination)) {
+		let stream = fs.createWriteStream(destination);
+		ytdl(item.url,{ filter: format => format.container === 'mp4' }).pipe(stream);
+		stream.on("close", ()=>{
+			playTrack('playing');
+		})
+	} else {
+		playTrack('playing');
+	}
+	printList('playing', 'currentList','display');
 }
 
 function search(string) {
-	let main = document.getElementById("results");
-	main.innerHTML = "<img src='resources/loading.gif' id='loading'>";
+	lists.search.items = [];
 	ytsr(string, {limit: 3}).then(result => {
 		result.items.forEach((item, i) => {
-			let button = document.createElement("BUTTON");
-			button.setAttribute("onClick",`loadVideo( '${item.link}' , '${item.title}' ) `);
-			button.setAttribute("class", "searchResult");
-			main.appendChild(button);
-			button.innerHTML=`<img class="thumbnail" src='${item.thumbnail}'>${item.title}`;
+			let track = {
+				title: item.title,
+				url: item.link,
+				thumbnail: item.thumbnail,
+				duration: item.duration,
+				playing: false,
+			};
+			lists.search.items.push(track);
 		});
-	document.getElementById("loading").style.display = "none";
+		printList('search','results','play');
 	});
+}
 
+function printList(list,target,action) {
+	let div = document.getElementById(target);
+	div.innerHTML = "";
+	lists[list].items.forEach((item, i) => {
+		let button = document.createElement("BUTTON");
+		switch (action) {
+			case 'play':
+				button.setAttribute("onClick",`loadVideo('search', ${i}, 'playing' ); `);
+				button.setAttribute("class", "searchResult");
+				div.appendChild(button);
+				button.innerHTML = (`<img class="thumbnail" src='${item.thumbnail}'>${item.title}`);
+				break;
+			case 'display':
+				button.setAttribute("onClick",`playTrack('playing',${i})`);
+				button.setAttribute("class", "playingTrack");
+				div.appendChild(button);
+				button.innerHTML = (`${item.title}`);
+				break;
+			}
+	});
 }
